eab2d983c204275890fff69fdd5722f9a50db8f7
martinj31 <julienmartinezf@gmail.com>
2023-04-29 20:25:11 +0200

    mini map fleche

--- Assets/Script/MiniMap.cs
+++ Assets/Script/MiniMap.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class MiniMap : MonoBehaviour
 {
-  /*  public GameObject cible;
-    public GameObject player;*/
+   public GameObject cible;
+    public GameObject player;
     public GameObject mapFleche;
+    private float rotationModifier = 90;
+    private float speed = 20;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
 
-       /* var heading = cible.transform.position - player.transform.position;
-        var distance = heading.magnitude;
-        var direction = heading / distance; // This is now the normalized direction.
-        */
+        Vector3 relativePos = cible.transform.position - player.transform.position;
+
+        float angle = Mathf.Atan2(relativePos.y, relativePos.x) * Mathf.Rad2Deg - rotationModifier;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        mapFleche.transform.rotation = Quaternion.Slerp(mapFleche.transform.rotation, rotation,Time.deltaTime * speed);
     }
 
 
