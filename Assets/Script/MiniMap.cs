9af7a03d894ac92aaf4f31effcbfaf829a591fff
martinj31 <julienmartinezf@gmail.com>
2023-04-29 17:31:41 +0200

    creation new scene

---
+++ Assets/Script/MiniMap.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+  /*  public GameObject cible;
+    public GameObject player;*/
+    public GameObject mapFleche;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       /* var heading = cible.transform.position - player.transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance; // This is now the normalized direction.
+        */
+    }
+
+
+}
